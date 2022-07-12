b4ca74655db647c086ea2b12388dc3f4158b5878
RinkM <mikerinkert@gmail.com>
2022-07-12 15:16:54 -0600

    removed notes

--- src/components/ClockFunctions.js
+++ src/components/ClockFunctions.js
@@ -5,32 +5,14 @@ import { Welcome } from "./welcome";
 // https://itnext.io/how-to-work-with-intervals-in-react-hooks-f29892d650f2
 
 
-
-// Function Templates:
-// function App() {
-//   return <div className="App">Hello World!</div>;
-// }
-
-// // OR (arrow-function syntax)
-
-// const App = () => {
-//   return <div className="App">Hello World!</div>;
-// };
-
-// // OR (implicit return)
-
-// const App = () => <div className="App">Hello World!</div>;
-
-// export default App;
-
-
 const ClockFunc = () => {
   const [person, setPerson] = useState("Adam");
+  const [creature, setCreature] = useState("Manx Cat")
     return(
 
-    <div className="App">
+    <div className="App">   
         Hello Fellow Clocks!
-        <h3><Welcome name = {person} animal = "Manx Cat"/></h3>
+        <h3><Welcome name = {person} animal = {creature}/></h3>
         <h4>The time is {new Date().toLocaleTimeString()}</h4>
         
     </div>
